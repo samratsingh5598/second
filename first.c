25f45691a1444a5e0e86b3834125a0fbb7ef7669
samratsingh5598 <samratsingh5598@gmail.com>
2026-08-22 09:00:15 +0530

    first file

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    printf(" hello world ");
+    int a = 10;
+    int b;
+    printf("Enter a number: ");
+    scanf("%d", &b);
+    printf(" sum of a and b is %d ", a + b);
+    getch();
+    return 0;
+}
